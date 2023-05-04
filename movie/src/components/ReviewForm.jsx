078854d1a7fd17984127e72cad6dbc5fc8a281c1
younhoso <thdbsgh3443@naver.com>
2023-05-04 11:00:18 +0900

    update: 하나의 state로 폼 구현하기2

--- movie/src/components/ReviewForm.jsx
+++ movie/src/components/ReviewForm.jsx
@@ -14,22 +14,6 @@ function ReviewForm() {
     [name]: value
    }))
   }
-  // const [title, setTitle] = useState('');
-  // const [rating, setRating] = useState(0);
-  // const [content, setContent] = useState('');
-
-  // const handleTitleChange = (e) => {
-  //   setTitle(e.target.value);
-  // };
-
-  // const handleRatingchange = (e) => {
-  //   const nextRating = Number(e.target.value) || 0;
-  //   setRating(nextRating);
-  // };
-
-  // const handleContentChange = (e) => {
-  //   setContent(e.target.value);
-  // };
 
   const handleSubmit = (e) => {
     e.preventDefault()
