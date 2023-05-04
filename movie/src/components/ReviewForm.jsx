66e0cf3133446d7f50e1bf04be3101c2da0bf47c
younhoso <thdbsgh3443@naver.com>
2023-05-04 10:59:40 +0900

    update: 하나의 state로 폼 구현하기

--- movie/src/components/ReviewForm.jsx
+++ movie/src/components/ReviewForm.jsx
@@ -1,28 +1,47 @@
 import React, { useState } from "react";
 
 function ReviewForm() {
-  const [title, setTitle] = useState('');
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState('');
+  const [values, setValues] = useState({
+    title: '',
+    rating: 0,
+    content: ''
+  })
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handleRatingchange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleChange = (e) => {
+   const { name, value } = e.target;
+   setValues((prevValues) => ({
+    ...prevValues,
+    [name]: value
+   }))
   }
+  // const [title, setTitle] = useState('');
+  // const [rating, setRating] = useState(0);
+  // const [content, setContent] = useState('');
+
+  // const handleTitleChange = (e) => {
+  //   setTitle(e.target.value);
+  // };
+
+  // const handleRatingchange = (e) => {
+  //   const nextRating = Number(e.target.value) || 0;
+  //   setRating(nextRating);
+  // };
+
+  // const handleContentChange = (e) => {
+  //   setContent(e.target.value);
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(values)
+  };
 
   return(
-    <form>
-      <input value={title} onChange={handleTitleChange} />
-      <input type="number" name="calorie" value={rating} onChange={handleRatingchange} />
-      <textarea name="content" value={content} onChange={handleContentChange}></textarea>
+    <form onSubmit={handleSubmit}>
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input type="number" name="rating" value={values.rating} onChange={handleChange} />
+      <textarea name="content" value={values.content} onChange={handleChange}></textarea>
+      <button type="submit">확인</button>
     </form>
   )
 };
