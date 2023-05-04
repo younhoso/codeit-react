0d9725f2bea5fe1f3aed82b412aee36aafce94d6
younhoso <thdbsgh3443@kakao.com>
2023-05-04 15:31:08 +0900

    update: 파일 인풋 초기화

--- foodit/src/components/FileInput.jsx
+++ foodit/src/components/FileInput.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useRef} from "react";
 
 function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextFile = e.target.files[0];
     onChange(name, nextFile)
   }
 
-  return <input type="file" onChange={handleChange}/>
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if(!inputNode) return;
+    inputNode.value = '';
+    onChange(name, null);
+  }
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef}/>
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  )
 }
 
 export default FileInput;
